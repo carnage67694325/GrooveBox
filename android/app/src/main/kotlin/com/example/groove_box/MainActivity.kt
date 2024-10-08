84319981f3fbfeeacdeefaa6be4bbb28dca8e91e
carnage67694325 <kingabdoeid2014@gmail.com>
2024-10-08 16:50:29 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/groove_box/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.groove_box
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class GrooveBoxApp extends StatelessWidget {
+  const GrooveBoxApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
