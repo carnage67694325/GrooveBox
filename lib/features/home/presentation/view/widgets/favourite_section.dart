abf3b66c64a349fb46f7eadec8f7ee6c80aaf415
carnage67694325 <kingabdoeid2014@gmail.com>
2024-10-09 12:35:48 +0300

    added favourite section

---
+++ lib/features/home/presentation/view/widgets/favourite_section.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:groove_box/core/utils/app_text_styles.dart';
+import 'package:groove_box/features/home/presentation/view/widgets/recently_played_list.dart';
+
+class FavouriteSection extends StatelessWidget {
+  const FavouriteSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SliverToBoxAdapter(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: 6),
+            child: Text(
+              'Favourite Songs',
+              style: AppTextStyles.heading,
+            ),
+          ),
+          RecentlyPlayedList(),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:groove_box/core/utils/app_gradient.dart';
 import 'package:groove_box/core/utils/app_text_styles.dart';
 import 'package:groove_box/core/utils/widgets/music_card.dart';
+import 'package:groove_box/features/home/presentation/view/widgets/favourite_section.dart';
 import 'package:groove_box/features/home/presentation/view/widgets/recently_played_list.dart';
+import 'package:groove_box/features/home/presentation/view/widgets/recently_played_widgets.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -19,18 +21,13 @@ class HomeViewBody extends StatelessWidget {
               height: 40,
             ),
           ),
+          RecentlyPlayedSection(),
           SliverToBoxAdapter(
-            child: Padding(
-              padding: EdgeInsets.only(left: 6),
-              child: Text(
-                'Recently Played',
-                style: AppTextStyles.heading,
-              ),
+            child: SizedBox(
+              height: 10,
             ),
           ),
-          SliverToBoxAdapter(
-            child: RecentlyPlayedList(),
-          )
+          FavouriteSection()
         ],
       ),
     );
