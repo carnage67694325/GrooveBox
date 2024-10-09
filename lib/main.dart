7214dcbd6888a943e846edea84d01e68d8e89a4b
carnage67694325 <kingabdoeid2014@gmail.com>
2024-10-09 23:50:36 +0300

    provided the cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:groove_box/core/simple_bloc_observer.dart';
 import 'package:groove_box/core/utils/app_router.dart';
+import 'package:groove_box/features/home/data/home_repo.dart';
+import 'package:groove_box/features/home/data/home_repo_imp.dart';
+import 'package:groove_box/features/home/presentation/view_model/get_music_cubit/get_music_cubit.dart';
 import 'package:media_store_plus/media_store_plus.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
+  Bloc.observer = SimpleBlocObserver();
   runApp(const GrooveBoxApp());
 }
 
@@ -12,18 +18,25 @@ class GrooveBoxApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: ThemeData.dark().copyWith(),
-      builder: (context, child) => ResponsiveBreakpoints.builder(
-        child: child!,
-        breakpoints: [
-          const Breakpoint(start: 0, end: 450, name: MOBILE),
-          const Breakpoint(start: 451, end: 800, name: TABLET),
-          const Breakpoint(start: 801, end: 1920, name: DESKTOP),
-          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
-        ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => GetMusicCubit(HomeRepoImp()),
+        )
+      ],
+      child: MaterialApp.router(
+        theme: ThemeData.dark().copyWith(),
+        builder: (context, child) => ResponsiveBreakpoints.builder(
+          child: child!,
+          breakpoints: [
+            const Breakpoint(start: 0, end: 450, name: MOBILE),
+            const Breakpoint(start: 451, end: 800, name: TABLET),
+            const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+            const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+          ],
+        ),
+        routerConfig: AppRouter.router,
       ),
-      routerConfig: AppRouter.router,
     );
   }
 }
